5447e01de8263599162a04bbc41c242467dde9c2
cjohirul <cjohirul71@gmail.com>
2020-10-23 23:15:17 -0400

    2nd atempt

--- Demo/src/main/java/homepage/TestClass.java
+++ Demo/src/main/java/homepage/TestClass.java
@@ -1,4 +1,10 @@
 package homepage;
 
 public class TestClass {
+
+    public static void main(String[] args) {
+        String ss = "Aryan";
+        StringBuffer xy = new StringBuffer(ss);
+        System.out.println(xy.reverse());
+    }
 }
--- Demo/src/main/java/homepage/TestClass1.java
+++ Demo/src/main/java/homepage/TestClass1.java
@@ -1,4 +1,12 @@
 package homepage;
 
 public class TestClass1 {
+    public static void main(String[] args) {
+
+
+        String s = "aryanayra";
+        StringBuffer xy = new StringBuffer(s);
+        System.out.println(xy.reverse());
+    }
+
 }
